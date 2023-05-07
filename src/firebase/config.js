cf4c23ca8f408c03e4b4c1154b8a01558da63e5a
Jean <jeanc16rlos@gmail.com>
2023-05-07 12:14:38 -0400

    🔧 implement Container and adapt env vars

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.GATSBY_DATABASE_URL,
-  projectId: process.env.GATSBY_PROJECT_ID,
-  storageBucket: process.env.GATSBY_STORAGE_BUCKET,
-  messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_GATSBY_DATABASE_URL,
+  projectId: process.env.REACT_APP_GATSBY_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_GATSBY_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_GATSBY_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AppContainer } from './containers/AppContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AppContainer> 
+      <App />
+    </AppContainer>
   </React.StrictMode>
 );
 
